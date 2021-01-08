53dcc8ee4f0cfd27a941bac13efadaf93620e9e7
Varun Sharma <varunsharma12@outlook.com>
2021-01-08 10:59:11 +0530

    😃 Added Forgot Password, Login, Register, Reset Password, Verify Email components.

---
+++ src/account/VerifyEmail.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import queryString from "query-string";
+import { accountService, alertService } from "../_services";
+
+const VerifyEmail = ({ history }) => {
+  const EmailStatus = {
+    Verifying: "Verifying",
+    Failed: "Failed",
+  };
+
+  const [emailStatus, setEmailStatus] = useState(EmailStatus.Verifying);
+
+  useEffect(() => {
+    const { token } = queryString.parse(window.location.search);
+
+    history.replace(window.location.pathname);
+
+    accountService
+      .verifyEmail(token)
+      .then(() => {
+        alertService.success("Verification successful, you can now login.", {
+          keepAfterRouteChange: true,
+        });
+        history.push("login");
+      })
+      .catch((error) => {
+        setEmailStatus(EmailStatus.Failed);
+      });
+  }, []);
+
+  function getBody() {
+    switch (emailStatus) {
+      case EmailStatus.Verifying:
+        return <div>Verifying...</div>;
+
+      case EmailStatus.Failed:
+        return (
+          <div>
+            Verification failed, you can also verify your account using the{" "}
+            <Link to="forgot-password">forgot password</Link> page.
+          </div>
+        );
+      default:
+        return "";
+    }
+  }
+
+  return (
+    <div>
+      <h3 className="card-header">Verify Email</h3>
+      <div className="card-body">{getBody()}</div>
+    </div>
+  );
+};
+
+export { VerifyEmail };
